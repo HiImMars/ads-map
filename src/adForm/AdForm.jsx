82b74ffea057ab32bcbb2e1c8a95536ca80d1db4
HiImMars <fedorenkorostyslav@gmail.com>
2024-01-08 18:23:33 +0200

    add demo links and fixed css

--- src/adForm/AdForm.jsx
+++ src/adForm/AdForm.jsx
@@ -48,7 +48,7 @@ const AdForm = ({ updateAds, ads }) => {
           formData
         );
 
-        Notify.success("You advertisement successfully posted");
+        Notify.success("Your advertisement successfully posted");
 
         updateAds((prevAds) => [...prevAds, postResponse.data]);
       } catch (error) {
@@ -65,68 +65,85 @@ const AdForm = ({ updateAds, ads }) => {
   };
 
   return (
-    <form onSubmit={handleSubmit} className={css.form}>
-      <h2 className={css.title}>Enter your ads info</h2>
-      <div className={css.inputWrapper}>
-        <label className={css.label} htmlFor="city">
-          City
-        </label>
-        <input
-          className={css.input}
-          id="city"
-          name="city"
-          type="text"
-          value={address}
-          onChange={(e) => setAddress(e.target.value)}
-          required
-        />
+    <aside className={css.adForm}>
+      <form onSubmit={handleSubmit} className={css.form}>
+        <h2 className={css.title}>Enter your ads info</h2>
+        <div className={css.inputWrapper}>
+          <label className={css.label} htmlFor="city">
+            City
+          </label>
+          <input
+            className={css.input}
+            id="city"
+            name="city"
+            type="text"
+            value={address}
+            onChange={(e) => setAddress(e.target.value)}
+            required
+          />
+        </div>
+        <div className={css.inputWrapper}>
+          <label className={css.label} htmlFor="title">
+            Title
+          </label>
+          <input
+            className={css.input}
+            id="title"
+            type="text"
+            name="title"
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+            required
+          />
+        </div>
+        <div className={css.inputWrapper}>
+          <label className={css.label} htmlFor="image">
+            Image
+          </label>
+          <input
+            className={css.input}
+            id="image"
+            type="text"
+            name="image"
+            value={image}
+            onChange={(e) => setImage(e.target.value)}
+            required
+          />
+        </div>
+        <div className={css.inputWrapper}>
+          <label className={css.label} htmlFor="price">
+            Price in UAH
+          </label>
+          <input
+            className={css.input}
+            id="price"
+            type="number"
+            name="price"
+            value={price}
+            onChange={(e) => setPrice(e.target.value)}
+            required
+          />
+        </div>
+        <button type="submit" className={css.button}>
+          Make an Advertisement
+        </button>
+      </form>
+      <div className={css.demoWrapper}>
+        <h3 className={css.demoTitle}>
+          <span className={css.warning}>Demo warning!</span> To add image to
+          your advertisement use one of the links below inside "Image" input.
+        </h3>
+        <p className={css.demoLink}>
+          https://images.pexels.com/photos/2775827/pexels-photo-2775827.jpeg?auto=compress&cs=tinysrgb&w=600
+        </p>
+        <p className={css.demoLink}>
+          https://images.pexels.com/photos/1524336/pexels-photo-1524336.jpeg?auto=compress&cs=tinysrgb&w=600
+        </p>
+        <p className={css.demoLink}>
+          https://images.pexels.com/photos/6032792/pexels-photo-6032792.jpeg?auto=compress&cs=tinysrgb&w=600
+        </p>
       </div>
-      <div className={css.inputWrapper}>
-        <label className={css.label} htmlFor="title">
-          Title
-        </label>
-        <input
-          className={css.input}
-          id="title"
-          type="text"
-          name="title"
-          value={title}
-          onChange={(e) => setTitle(e.target.value)}
-          required
-        />
-      </div>
-      <div className={css.inputWrapper}>
-        <label className={css.label} htmlFor="image">
-          Image
-        </label>
-        <input
-          className={css.input}
-          id="image"
-          type="text"
-          name="image"
-          value={image}
-          onChange={(e) => setImage(e.target.value)}
-          required
-        />
-      </div>
-      <div className={css.inputWrapper}>
-        <label className={css.label} htmlFor="price">
-          Price in UAH
-        </label>
-        <input
-          className={css.input}
-          id="price"
-          type="number"
-          name="price"
-          value={price}
-          onChange={(e) => setPrice(e.target.value)}
-          required
-        />
-      </div>
-      <button type="submit" className={css.button}>
-        Make an Advertisement
-      </button>
-    </form>
+    </aside>
   );
 };
 
